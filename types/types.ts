1e6819986edf27182557ba1d966f001f764908a8
Ivona Josipovic <josipovic.ivona@gmail.com>
2025-09-14 17:53:16 +0200

    Adding types

---
+++ types/types.ts
@@ -0,0 +1,11 @@
+export interface Record {
+  lat: number
+  lon: number
+  temperature_c: number
+  windspeed: number
+  winddirection: number
+  weathercode: number
+  observed_at: Date
+  test_data: number
+  device_id: number
+}
